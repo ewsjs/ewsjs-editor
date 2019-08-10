66ba4be3729f97754ed6fc5f56d9ec4bf430b743
Gautam Singh <gautamsi@users.noreply.github.com>
2019-08-10 13:49:50 +0530

    wip

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Stack, Text, Modal } from 'office-ui-fabric-react';
+
+export default () => (
+  <Stack horizontal padding={10}>
+    <Stack.Item>Tree Component</Stack.Item>
+    <Stack.Item grow>Table Component</Stack.Item>
+  </Stack>
+);
